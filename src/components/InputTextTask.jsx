183e5ea1b2bd893b6d031029e1138b3e4bca1c66
Garekox <garekox@protonmail.com>
2023-01-09 23:35:27 -0300

    Update InputTextTask.jsx

--- src/components/InputTextTask.jsx
+++ src/components/InputTextTask.jsx
@@ -1,4 +1,4 @@
 const InputTextTask = ( { handleInput } ) => {
   return <input onChange={handleInput} type="text" placeholder="Type here" className="input input-bordered w-3/4" />
 }
-export default
+export default InputTextTask;
